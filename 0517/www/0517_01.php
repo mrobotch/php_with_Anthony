e91a85460ff9e63a0ec082d3cc2a7f1279bdc7ce
mrobotch <mrobotch@gmail.com>
2021-05-17 10:46:40 +0900

    madlib練習

---
+++ 0517/www/0517_01.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <title>calculation</title>
+  </head>
+  <body>
+    <form action="0517_01.php" method="get">
+      Color: <input type ="text" name="color"><br>
+      Plural Noun: <input type="text" name="pluralNoun"><br>
+      Celebrity: <input type="text" name="celebrity"><br>
+      <input type="submit">
+    </form>
+<br><br>
+  <?php
+  $color = $_GET["color"];
+  $pluralNoun ="$_GET[pluralNoun]";
+  $celebrity ="$_GET[celebrity]";
+
+  echo "Roses are $color,<br>";
+  echo "$pluralNoun are blue,<br>";
+  echo "I love $celebrity.";
+
+   ?>
+
+  </body>
+</html>
