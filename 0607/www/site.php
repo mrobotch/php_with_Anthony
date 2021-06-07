0e06a49cb26c10fb75c7a8985f05fabc415108a7
mrobotch <mrobotch@gmail.com>
2021-06-07 10:09:16 +0900

    Array

---
+++ 0607/www/site.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <title>POST</title>
+  </head>
+  <body>
+<?php
+ $friends = array("Kevin", "Karen", "Oscar", "Jim");
+ $friends[10] = "Mike";
+echo "$friends[10]<br>";
+echo count($friends);
+
+?>
+
+
+  </body>
+</html>
