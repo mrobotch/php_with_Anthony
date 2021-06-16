05401820deb9da70ce5ec38cf7dfdf4486389bc2
mrobotch <mrobotch@gmail.com>
2021-06-16 10:27:26 +0900

    notesを変更

---
+++ 1_5_Review/site_2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <title>variable</title>
+  </head>
+
+  <body>
+    <?php
+$characterName1 = "Mikiko";
+$characterName2 = "Anthony";
+$characterAge = 27;
+
+echo "There once was a woman named $characterName1.<br>";
+echo "She was $characterAge years old.<br>";
+echo "She didn't like the name $characterName1. <br>";
+echo "She wanted to change her name $characterName2.";
+ ?>
+
+
+  </body>
